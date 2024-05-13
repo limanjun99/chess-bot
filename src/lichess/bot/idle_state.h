e1f42137625c0743a12d854b7050743f208d2647
An Jun <limanjun99@gmail.com>
2024-05-13 23:46:15 +0800

    chore: add back bot challenging logic

--- src/lichess/bot/idle_state.h
+++ src/lichess/bot/idle_state.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <chrono>
+
 #include "bot_state.h"
 
 // Bot is idling, it should periodically challenge other bots, and accept all incoming challenges.
@@ -10,4 +12,11 @@ public:
   virtual std::optional<std::unique_ptr<BotState>> handle_challenge(const json& challenge) override;
 
   virtual std::optional<std::unique_ptr<BotState>> handle_null_event() override;
+
+private:
+  std::chrono::time_point<std::chrono::steady_clock> idle_since;
+
+  bool is_ready_to_challenge() const;
+
+  std::optional<std::unique_ptr<BotState>> issue_challenge();
 };
