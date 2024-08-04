372ecda6bf0ce6e50c3a284b02718c8afa8864d2
An Jun <limanjun99@gmail.com>
2024-08-04 13:00:39 +0800

    refactor: killer moves

--- src/engine/src/killer_moves.cpp
+++ src/engine/src/killer_moves.cpp
@@ -1,24 +1,21 @@
 #include "killer_moves.h"
 
-void KillerMoves::add(const chess::Move& move, int depth_left) {
-  for (int i = 0; i < KillerMoves::count; i++) {
-    if (killer_moves[depth_left][i] == move) {
-      std::swap(killer_moves[depth_left][i], killer_moves[depth_left][0]);
-      return;
-    }
-  }
+#include <algorithm>
 
-  for (int i = KillerMoves::count - 1; i > 0; i--) {
-    killer_moves[depth_left][i] = killer_moves[depth_left][i - 1];
+void KillerMoves::add(const chess::Move& move, int depth_left) {
+  auto& moves = killer_moves[depth_left];
+  const auto it = std::ranges::find(moves, move);
+  if (it == moves.end()) {
+    std::move_backward(moves.begin(), moves.end() - 1, moves.end());
+  } else {
+    std::move_backward(moves.begin(), it, it + 1);
   }
-  killer_moves[depth_left][0] = move;
+  moves[0] = move;
 }
 
 void KillerMoves::clear() {
-  for (int i = 0; i < config::max_depth; i++) {
-    for (int j = 0; j < KillerMoves::count; j++) {
-      killer_moves[i][j] = chess::Move::null();
-    }
+  for (auto& array : killer_moves) {
+    std::ranges::fill(array, chess::Move::null());
   }
 }
 
